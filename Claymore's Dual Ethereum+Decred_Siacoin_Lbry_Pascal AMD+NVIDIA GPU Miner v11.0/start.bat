bd0cd94ad818f7582fd0a69d4ef17b590573f649
Atlanta Strippers R' Us <34319821+cmoore0362@users.noreply.github.com>
2018-03-17 01:41:35 -0400

    Add files via upload

---
+++ Claymore's Dual Ethereum+Decred_Siacoin_Lbry_Pascal AMD+NVIDIA GPU Miner v11.0/start.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+ethminer.exe --farm-recheck 200 -G -S eth-east1.nanopool.org:9999 -O ewal 0x56B25F0aB22C949917CeA1D10aE50BB3B401d7b4.rig3 - talesfromthecryptocoins@gmail.com
